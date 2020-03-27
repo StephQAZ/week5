f71e5f951cfadfa228255d85963c1b67988fb3fa
桐 孙 <2731881741@qq.com>
2020-03-27 14:44:21 +0800

    练习5

--- week5/week5.cpp
+++ week5/week5.cpp
@@ -3,15 +3,49 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
 using namespace cv;
-
+using namespace std;
 int main()
 {
-	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\curry.jpg",0);
-	imshow("curry", srcMat);
-	waitKey(0);
-	return 0;
+	VideoCapture cap(0);
+	double scale = 0.5;
+	double i_minH = 0;
+	double i_maxH = 20;
+
+	double i_minS = 43;
+	double i_maxS = 255;
+
+	double i_minV = 55;
+	double i_maxV = 255;
+
+	while (1)
+	{
+		Mat frame;
+		Mat hsvMat;
+		Mat detectMat;
+		Mat medMat;
+		Mat outMat;
+
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols * scale, frame.rows * scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		rFrame.copyTo(detectMat);
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+
+		medianBlur(rFrame, medMat, 15);
+
+		rFrame.copyTo(outMat);
+		medMat.copyTo(outMat, detectMat);
+
+		imshow("检测图像", detectMat);
+		imshow("原图", rFrame);
+		imshow("磨皮", outMat);
+		waitKey(30);
+	}
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
